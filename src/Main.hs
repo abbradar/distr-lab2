d802341bab7c2fb5d5661bed83f4a32b41a82c18
Nikolay Amiantov <ab@fmap.me>
2015-01-20 07:18:18 +0300

    Added refresh and server-side tokens in general

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,8 @@ import Control.Monad
 import Data.Maybe
 import Text.Read (readMaybe)
 import Data.Typeable
-import Control.Applicative ((<$>), (<*>))
+import Data.Monoid
+import Control.Applicative
 import Data.IORef
 import Data.Tuple
 import GHC.Generics (Generic)
@@ -12,20 +13,22 @@ import Control.Monad.Trans.Class
 import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)
 import Data.Time.Clock
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Base64 as B64'
 import qualified Data.ByteString.Base64.URL as B64
 import qualified Data.ByteString.Lazy as BL
 import Crypto.Random
-import Data.Aeson
+import Data.Aeson hiding (Value)
 import qualified Data.Aeson.Types as AT
 import qualified Data.Vector as V
-import qualified Database.Esqueleto as E
+import Database.Esqueleto
 import qualified Database.Esqueleto.Internal.Language as E
 import qualified Database.Esqueleto.Internal.Sql as E
-import Database.Persist as DB
+import qualified Database.Persist as D
+import qualified Database.Persist.Sql as D
 import Database.Persist.TH
-import Database.Persist.Sql
-import Yesod.Core
+import Yesod.Core hiding (Value)
 import Yesod.Auth
 import Yesod.Persist (YesodPersist(..), get404, defaultRunDB)
 import Yesod.Form
@@ -52,6 +55,13 @@ share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
     key Text
     user UserId
     UniqueClient key
+
+  Token json
+    client ClientId
+    user UserId
+    until UTCTime
+    code Text
+    UniqueToken code
 
   Package
     name Text
@@ -93,28 +103,20 @@ mkYesod "App" [parseRoutes|
   / HomeR GET
   /auth AuthR Auth getAuth
   /user/me UserMeR GET
+  /user/me/cleartokens UserClearTokensR POST
   /user/new UserNewR GET POST
   /status StatusR GET
   /oauth OAuthR GET POST
   /oauth/reply OAuthReplyR POST
   /clients ClientsR GET
-  /clients/new ClientNewR POST
-  !/clients/#ClientId ClientR GET
+  !/clients/new ClientNewR POST
+  /clients/#ClientId ClientR GET
   /packages PackagesR GET
-  !/packages/#PackageId PackageR GET
+  /packages/#PackageId PackageR GET
   /groups GroupsR GET
-  !/groups/#GroupId GroupR GET
+  /groups/#GroupId GroupR GET
   /groups/#GroupId/packages GroupPackagesR GET
 |]
-
-data OAuthToken = OAuthToken { tokenUntil :: UTCTime
-                             , tokenClient :: ClientId
-                             , tokenUser :: UserId
-                             }
-                deriving (Generic)
-
-instance ToJSON OAuthToken
-instance FromJSON OAuthToken
 
 instance YesodAuth App where
   type AuthId App = UserId
@@ -124,12 +126,14 @@ instance YesodAuth App where
   loginDest _ = HomeR
   logoutDest _ = HomeR
 
+-- TODO: Total counts
+
 -- | Like 'requireAuthId', but also accepts OAuth2 authentification.
 requireAnyAuthId :: Handler UserId
-requireAnyAuthId = runMaybeT (msum $ map MaybeT [ maybeAuthId
+requireAnyAuthId = maybeM (permissionDenied "Not authorized") $
+                   runMaybeT (msum $ map MaybeT [ maybeAuthId
                                               , maybeOAuthId
                                               ])
-                   >>= maybeM (permissionDenied "Not authorized")
 
 instance YesodPersist App where
   type YesodPersistBackend App = SqlBackend
@@ -146,30 +150,28 @@ oauthError e = sendResponseStatus H.badRequest400 $ object [ "error" .= e ]
 encryptJSON :: ToJSON a => a -> Handler Text
 encryptJSON obj = do
   app <- getYesod
-  liftIO $ decodeUtf8 <$> CS.encryptIO (csKey app) (B.concat $ BL.toChunks $ encode obj)
+  liftIO $ decodeUtf8 <$> B64.encode <$> B64'.decodeLenient <$> CS.encryptIO (csKey app) (B.concat $ BL.toChunks $ encode obj)
 
 decryptJSON :: FromJSON a => Text -> Handler (Maybe a)
 decryptJSON t = do
   app <- getYesod
-  return $ CS.decrypt (csKey app) (encodeUtf8 t) >>= decodeStrict
+  return $ CS.decrypt (csKey app) (B64'.encode $ B64.decodeLenient $ encodeUtf8 t) >>= decodeStrict
 
 maybeOAuthId :: Handler (Maybe UserId)
 maybeOAuthId = runMaybeT $ do
-  auth <- MaybeT $ lookupHeader "Authorization"
-  let (t, T.strip -> access) = T.breakOn " " $ decodeUtf8 auth
+  (t, T.strip -> access) <- T.breakOn " " <$> decodeUtf8 <$> MaybeT (lookupHeader "Authorization")
   guard $ t == "bearer"
   lift $ do
-    OAuthToken { .. } <- decryptJSON access >>= maybeM (oauthError "invalid_request")
-    good <- all (/= 0) <$> runDB (sequence [ count [UserId ==. tokenUser]
-                                           , count [ClientId ==. tokenClient]
-                                           ])
-    now <- liftIO getCurrentTime
-    unless (good && now <= tokenUntil) $ oauthError "invalid_client"
-    return tokenUser
+    OAuthAccess id <- maybeM (oauthError "invalid_request") $ decryptJSON access
+    maybeM (oauthError "invalid_client") $ runMaybeT $ do
+      Token {..} <- MaybeT $ runDB $ get id
+      now <- liftIO getCurrentTime
+      guard $ now < tokenUntil
+      return tokenUser
 
 makeApp :: AppConfig DefaultEnv () -> IO App
 makeApp settings = do
-  dbConf <- withYamlEnvironment "config/postgresql.yml" (appEnv settings) DB.loadConfig >>= DB.applyEnv
+  dbConf <- withYamlEnvironment "config/postgresql.yml" (appEnv settings) D.loadConfig >>= D.applyEnv
   appPool <- createPoolConfig dbConf
   runSqlPool (runMigration migrateAll) appPool
   entropy <- createEntropyPool
@@ -179,7 +181,7 @@ makeApp settings = do
   return App { .. }
 
 main :: IO ()
-main = defaultMain Y.loadDevelopmentConfig (makeApp >=> toWaiApp)
+main = defaultMain Y.loadDevelopmentConfig $ makeApp >=> toWaiApp
 
 getHomeR = do
   maid <- maybeAuth
@@ -204,7 +206,7 @@ newUserForm = renderDivs $ User
     <*> areq passwordField "Password" Nothing
     <*> areq emailField "E-Mail" Nothing
   where unique n = do
-          clients <- runDB $ count [ UserName ==. n]
+          clients <- runDB $ D.count [UserName D.==. n]
           return $ if clients /= 0 then Left ("User already exists!" :: Text) else Right n 
 
 newUser widget enctype = do
@@ -230,59 +232,81 @@ postUserNewR = do
        |]
    _ -> newUser widget enctype
 
+-- We use this to apply CSRF protection
+emptyForm = renderDivs $ pure ()
+
 getUserMeR = do
   uid <- requireAnyAuthId
   Just user <- runDB $ get uid
+  (widget, enctype) <- generateFormPost emptyForm
   defaultLayoutJson
     [whamlet|
       <p>User name: #{userName user}
       <p>User email: #{userEmail user}
+      <p><form method=post action=@{UserClearTokensR} enctype=#{enctype}>
+        ^{widget}
+        <button>Clear access tokens
     |]
     $ return $ object [ "name" .= userName user
                       , "email" .= userEmail user
                       ]
 
+postUserClearTokensR = do
+  uid <- requireAuthId
+  ((FormSuccess (), _), _) <- runFormPost emptyForm
+  runDB $ D.deleteWhere [TokenUser D.==. uid]
+  defaultLayout
+    [whamlet|
+      <p>Your tokens have been cleared.
+      <p><a href=@{UserMeR}>Return to my user page
+    |]
+
 getStatusR = do
-  c <- runDB $ count ([] :: [Filter User])
+  c <- runDB $ D.count ([] :: [D.Filter User])
   defaultLayoutJson
     [whamlet|
       <p>Total users: #{c}
     |]
     $ return $ object [ "total_users" .= c ]
 
-getSomethings :: (E.From E.SqlQuery E.SqlExpr E.SqlBackend a1, E.SqlSelect a2 a) =>
-                 Route App -> (a1 -> E.SqlQuery a2) -> (Widget -> Handler Html) -> (a -> Widget) -> (a -> [AT.Pair]) -> Handler TypedContent
 getSomethings route query layout pitem jitem = do
   let getI d n = fromMaybe d <$> (>>= readMaybe . T.unpack) <$> lookupGetParam n
   start <- getI 0 "start"
-  count <- getI 10 "count"
-  items' <- runDB $ E.select $ E.from $ \u -> do
-    E.limit $ fromIntegral $ count + 1
-    E.offset $ fromIntegral start
-    query u
-  let items = take count items'
+  num <- getI 10 "count"
+  (items, n :: Int) <- runDB $ do
+    items <- select $ from $ \u -> do
+      offset $ fromIntegral start
+      limit $ fromIntegral num
+      query u
+    [Value n] <- select $ from $ \n -> query n >> return countRows
+    return (items, n)
+
   selectRep $ do
-    provideRep $ do
-      layout [whamlet|
-              <ul>
-                $forall e <- items
-                  <li>^{pitem e}
-                $if start /= 0
-                  <p><a href="@{route}?start=0&count=#{count}">First page
-                $if length items' > count
-                  <p><a href="@{route}?start=#{start + count}&count=#{count}">Next page
-                $if start /= 0
-                  <p><a href="@{route}?start=#{min 0 (start - count)}&count=#{count}">Previous page
-             |]
-    provideRep $ return $ Array $ V.fromList $ map (object . jitem) items
+    provideRep $ layout
+      [whamlet|
+        <p>Total items: #{n}
+        <ul>
+          $forall e <- items
+            <li>^{pitem e}
+        $if n >= (start + num)
+          <p><a href="@{route}?start=#{start + num}&count=#{num}">Next page
+        $if start /= 0
+          <p><a href="@{route}?start=#{min 0 (start - num)}&count=#{num}">Previous page
+          <p><a href="@{route}?start=0&count=#{num}">First page
+      |]
+    provideRep $ return $ object [ "total" .= n
+                                 , "items" .= map (object . jitem) items
+                                 ]
 
 getClientsR = do
   uid <- requireAuthId
-  getSomethings ClientsR (\x -> E.where_ (x E.^. ClientUser E.==. E.val uid) >> return x)
+  (widget, enctype) <- generateFormPost emptyForm
+  getSomethings ClientsR (\x -> where_ (x ^. ClientUser ==. val uid) >> return x)
     (\w -> defaultLayout
            [whamlet|
-             <form method=post action=@{ClientNewR}>
-              <button>New client
+             <form method=post action=@{ClientNewR} enctype=#{enctype}>
+               ^{widget}
+               <button>New client
              ^{w}
            |])
     (\(Entity id client) -> [whamlet|<a href=@{ClientR id}>#{clientKey client}|])
@@ -297,10 +321,15 @@ getClientR cid = do
       <p>Client ID: #{clientKey c}
     |]
 
+randomA :: Int -> Handler ByteString
+randomA n = do
+  app <- getYesod
+  liftIO $ atomicModifyIORef (randGen app) $ swap . cprgGenerate n
+
 postClientNewR = do
   clientUser <- requireAuthId
-  app <- getYesod
-  clientKey <- decodeUtf8 <$> B64.encode <$> liftIO (atomicModifyIORef (randGen app) $ swap . cprgGenerate 16)
+  ((FormSuccess (), _), _) <- runFormPost emptyForm
+  clientKey <- decodeUtf8 <$> B64.encode <$> randomA 16
   cid <- runDB $ insert Client { .. }
   defaultLayout
     [whamlet|
@@ -324,9 +353,9 @@ getPackageR cid = do
   (c, p, gs) <- runDB $ do
     c <- get404 cid
     p <- maybe (return Nothing) (\i -> Just <$> Entity i <$> fromJust <$> get i) $ packageReplacedBy c
-    gs <- E.select $ E.from $ \(gi, g) -> do
-      E.where_ (gi E.^. PackageGroupPackage E.==. E.val cid)
-      E.where_ (gi E.^. PackageGroupGroup E.==. g E.^. GroupId)
+    gs <- select $ from $ \(gi, g) -> do
+      where_ $ gi ^. PackageGroupPackage ==. val cid
+      where_ $ gi ^. PackageGroupGroup ==. g ^. GroupId
       return g
     return (c, p, gs)
   defaultLayoutJson
@@ -374,9 +403,9 @@ getGroupR cid = do
                         ]
 
 getGroupPackagesR i = getSomePackages (GroupPackagesR i) $
-                      \p -> E.from $ \gi -> do
-                        E.where_ (gi E.^. PackageGroupGroup E.==. E.val i)
-                        E.where_ (gi E.^. PackageGroupPackage E.==. p E.^. PackageId)
+                      \p -> from $ \gi -> do
+                        where_ $ gi ^. PackageGroupGroup ==. val i
+                        where_ $ gi ^. PackageGroupPackage ==. p ^. PackageId
                         return p
 
 data OAuthRequest = OAuthRequest { reqClient :: ClientId
@@ -391,12 +420,6 @@ instance FromJSON OAuthRequest
 instance PathPiece OAuthRequest where
   fromPathPiece = decodeStrict . B64.decodeLenient . encodeUtf8
   toPathPiece = decodeUtf8 . B64.encode . B.concat . BL.toChunks . encode
-
-data OAuthCode = OAuthCode URL OAuthToken
-               deriving (Generic)
-
-instance ToJSON OAuthCode
-instance FromJSON OAuthCode
 
 allowForm r = renderDivs $ (,)
               <$> areq checkBoxField "Allow access for this client" (Just False)
@@ -413,9 +436,9 @@ getOAuthR = do
   reqURL <- shouldBe checkURL lookupGetParam "redirect_uri"
   let reqError :: String -> Handler a
       reqError t = redirect $ add_param reqURL ("error", t)
-  cid <- getCid >>= maybeM (reqError "invalid_request")
-  Entity reqClient _ <- runDB (getBy $ UniqueClient cid) >>=
-                        maybeM (reqError "unauthorized_client")
+  cid <- maybeM (reqError "invalid_request") $ getCid
+  Entity reqClient _ <- maybeM (reqError "unauthorized_client") $
+                        runDB $ getBy $ UniqueClient cid
   void requireAuthId
   reqState <- lookupGetParam "state"
   (widget, enctype) <- generateFormPost $ allowForm $ Just OAuthRequest { .. }
@@ -426,33 +449,81 @@ getOAuthR = do
         <button>Proceed
     |]
 
+data OAuthCode = OAuthCode { codeURL :: URL
+                           , codeId :: TokenId
+                           }
+               deriving (Generic)
+
+instance ToJSON OAuthCode
+instance FromJSON OAuthCode
+
+data OAuthAccess = OAuthAccess { accessId :: TokenId }
+               deriving (Generic)
+
+instance ToJSON OAuthAccess
+instance FromJSON OAuthAccess
+
+data OAuthRefresh = OAuthRefresh { refreshId :: TokenId }
+                  deriving (Generic)
+
+instance ToJSON OAuthRefresh
+instance FromJSON OAuthRefresh
+
 postOAuthReplyR = do
   tokenUser <- requireAuthId
   ((FormSuccess (answer, OAuthRequest { .. }), _), _) <- runFormPost $ allowForm Nothing
   void $ if answer
     then do
-      app <- getYesod
-      tokenUntil <- addUTCTime (30 * 60) <$> liftIO getCurrentTime
-      code <- encryptJSON $ OAuthCode reqURL OAuthToken { tokenClient = reqClient
-                                                          , .. }
+      tokenUntil <- addUTCTime (10 * 60) <$> liftIO getCurrentTime
+      tokenCode <- decodeUtf8 <$> B64.encode <$> randomA 16
+      id <- runDB $ insert Token { tokenClient = reqClient
+                                 , ..
+                                 }
+      code <- encryptJSON $ OAuthCode reqURL id
       redirect $ foldl add_param reqURL $ [("code", T.unpack code)]
         ++ maybe [] (\s -> [("state", T.unpack s)]) reqState
     else redirect $ add_param reqURL ("error", "access_denied")
 
-postOAuthR = do
-  let getCode = runMaybeT $ do
-        rtype <- MaybeT $ lookupPostParam "grant_type"
-        guard $ rtype == "authorization_code"
-        code <- MaybeT (lookupPostParam "code") >>= MaybeT . decryptJSON
-        url <- MaybeT $ (>>= importURL . T.unpack) <$> lookupPostParam "redirect_uri"
-        cid <- MaybeT $ lookupPostParam "client_id"
-        Entity client _ <- MaybeT $ runDB $ getBy $ UniqueClient cid
-        return (code, client, url)
-  (OAuthCode turl token@OAuthToken { .. }, client, url) <- getCode >>= maybeM (oauthError "invalid_request")
+postOAuthR = maybeM (oauthError "invalid_request") $ runMaybeT $ do
   now <- liftIO getCurrentTime
-  unless (turl == url && tokenClient == client && now < tokenUntil) $ oauthError "invalid_client"
-  access <- encryptJSON token
-  return $ object [ "token_type" .= ("bearer" :: Text)
-                  , "access_token" .= access
-                  , "expires_in" .= (tokenUntil `diffUTCTime` now)
-                  ]
+  let expireTime = addUTCTime (30 * 60) now
+  id <- MaybeT (lookupPostParam "grant_type") >>= \case
+    "authorization_code" -> do
+      code <- MaybeT $ lookupPostParam "code"
+      OAuthCode turl tid <- MaybeT $ decryptJSON code
+      url <- MaybeT $ (>>= importURL . T.unpack) <$> lookupPostParam "redirect_uri"
+      cid <- MaybeT $ lookupPostParam "client_id"
+      Entity client _ <- MaybeT $ runDB $ getBy $ UniqueClient cid
+      lift $ maybeM (oauthError "invalid_client") $ runDB $ get tid >>= \case
+        Nothing -> D.deleteWhere [TokenCode D.==. code] >> return Nothing
+        Just Token {..} | turl == url && tokenClient == client && now < tokenUntil -> do
+                            D.delete tid
+                            Just <$> insert Token { tokenUntil = expireTime
+                                                  , ..
+                                                  }
+                        | otherwise -> return Nothing
+
+    "refresh_token" -> do
+      refresh <- MaybeT $ lookupPostParam "refresh_token"
+      liftIO $ putStrLn $ T.unpack refresh
+      OAuthRefresh id <- MaybeT $ decryptJSON refresh
+      lift $ do
+        maybeM (oauthError "invalid_client") $ runDB $ runMaybeT $ do
+          Token {..} <- MaybeT $ get id
+          lift $ do
+            D.delete id
+            insert Token { tokenUntil = expireTime
+                         , ..
+                         }
+
+    _ -> mzero
+
+  lift $ do
+    access <- encryptJSON $ OAuthAccess id
+    refresh <- encryptJSON $ OAuthRefresh id
+    liftIO $ putStrLn $ T.unpack refresh
+    return $ object [ "token_type" .= ("bearer" :: Text)
+                    , "access_token" .= access
+                    , "refresh_token" .= refresh
+                    , "expires_in" .= (expireTime `diffUTCTime` now)
+                    ]
--- src/Utils.hs
+++ src/Utils.hs
@@ -25,13 +25,13 @@ instance ToJSON URL where
   toJSON = String . T.pack . exportURL
 
 instance FromJSON URL where
-  parseJSON (String t) = maybeM mzero $ importURL $ T.unpack t
+  parseJSON (String t) = maybeM mzero $ return $ importURL $ T.unpack t
   parseJSON _ = mzero
 
 -- | Convenience function for various "maybe get" things
 shouldBe :: (a -> Maybe b) -> (Text -> HandlerT site IO (Maybe a)) -> Text -> HandlerT site IO b
-shouldBe t f n = ((>>= t) <$> f n) >>= maybeM (invalidArgs [n])
+shouldBe t f n = maybeM (invalidArgs [n]) $ ((>>= t) <$> f n)
 
--- | 'fromMaybe', lifted for monadic actions.
-maybeM :: Monad m => m a -> Maybe a -> m a
-maybeM d = maybe d return
+-- | 'fromMaybe' for monads with convenient signature
+maybeM :: Monad m => m a -> m (Maybe a) -> m a
+maybeM d m = m >>= maybe d return
